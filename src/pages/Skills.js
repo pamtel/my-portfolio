ee1e317816e8102bf13d3fb7a89c8747d306942e
pamtel <pamelaakosahiante@gmail.com>
2021-06-08 17:38:04 +0100

    updated

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,15 +1,21 @@
-import React from "react";
+import { useEffect } from "react";
 import { skillsData } from "../Components/skillsData";
 import Tittle from "../Components/Tittle";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function Skills() {
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+  }, []);
+
   return (
     <div className="SkillsPage">
       <Tittle title={"My Skills"} span={"My Skills"} />
       <div className="skillsContainer">
         {skillsData.map((data, index) => {
           return (
-            <div key={index} className="skill">
+            <div key={index} className="skill" data-aos="zoom-out">
               <h4>{data.skill}</h4>
             </div>
           );
