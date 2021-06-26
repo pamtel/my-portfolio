4908b01c1891f5aed1c1b891e5092fd5bb26efce
pamtel <pamelaakosahiante@gmail.com>
2021-06-26 15:34:44 +0100

    made changes on the portfolio page

--- src/Components/allPortfolios.js
+++ src/Components/allPortfolios.js
@@ -2,10 +2,22 @@ import port1 from "../img/portImages/port1.png";
 import port2 from "../img/portImages/port2.png";
 import port3 from "../img/portImages/port3.png";
 import port4 from "../img/portImages/port4.png";
+import port5 from "../img/portImages/port5.png";
 
 const portfolios = [
   {
     id: 1,
+    category: "JavaScript",
+    link1: "https://github.com/pamtel/Billic_Store",
+    link2: "https://billic-store.netlify.app/",
+    icon1: "G",
+    icon2: "N",
+    image: port5,
+    title:
+      "This website is built with html, css and jquery",
+  },
+  {
+    id: 2,
     category: "Mernstack",
     link1: "https://github.com/pamtel/mern-shopping-cart-client",
     link2: "https://mern-shopping-cart-client.netlify.app/",
@@ -16,7 +28,7 @@ const portfolios = [
       "Fullstack Ecommerce site built with react, redux, mongodb, node & express",
   },
   {
-    id: 2,
+    id: 3,
     category: "JavaScript",
     link1: "https://github.com/pamtel/My-movie-app",
     link2: "https://pamtel-movie-app.netlify.app",
@@ -26,7 +38,7 @@ const portfolios = [
     title: "Movie App built with vanilla javascript",
   },
   {
-    id: 3,
+    id: 4,
     category: "css",
     link1: "https://github.com/pamtel/knock-lounge",
     link2: "https://pamtel-lounge-website.netlify.app",
@@ -36,7 +48,7 @@ const portfolios = [
     title: "Website built with html and css",
   },
   {
-    id: 4,
+    id: 5,
     category: "React",
     link1: "https://github.com/pamtel/ecommerce",
     link2: "https://pamtel-ecommerce.netlify.app/",
